556263cdb376c147393230089e8de81dda8816db
m.kachalov <mike74@mail.ru>
2022-09-27 19:33:56 +0300

    Начальный комит

---
+++ application/schr-service/schr-service-application/src/main/java/moscow/ptnl/schr/servlet/InfoServlet.java
@@ -0,0 +1,25 @@
+package moscow.ptnl.schr.servlet;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.stream.Collectors;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author sorlov
+ */
+@RestController
+public class InfoServlet {
+
+    @GetMapping("info.json")
+    public String info() {
+        InputStream resource = getClass().getClassLoader().getResourceAsStream("info.json");
+        return new BufferedReader(new InputStreamReader(resource, StandardCharsets.UTF_8))
+                .lines()
+                .collect(Collectors.joining("\n"));
+
+    }
+}
