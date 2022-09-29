27c1b6f7bc8418915b7dd13934edb14395aa9ef8
m.kachalov <mike74@mail.ru>
2022-09-29 19:29:34 +0300

    SCHREGISTER-2 Создание каркаса сервиса - поддержка Elasticsearch

---
+++ application/schr-service/schr-service-application/src/main/java/moscow/ptnl/schr/configuration/ESConfiguration.java
@@ -0,0 +1,57 @@
+package moscow.ptnl.schr.configuration;
+
+import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import co.elastic.clients.json.jackson.JacksonJsonpMapper;
+import co.elastic.clients.transport.ElasticsearchTransport;
+import co.elastic.clients.transport.rest_client.RestClientTransport;
+import org.apache.http.HttpHost;
+import org.elasticsearch.client.RestClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.elasticsearch.client.elc.ElasticsearchTemplate;
+import org.springframework.data.elasticsearch.config.ElasticsearchConfigurationSupport;
+import org.springframework.data.elasticsearch.core.convert.ElasticsearchConverter;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+
+/**
+ *
+ * @author m.kachalov
+ */
+@Configuration
+@EnableElasticsearchRepositories(
+    basePackages = "moscow.ptnl.schr.repository.es"
+)
+@PropertySource("classpath:elasticsearch.properties")
+public class ESConfiguration extends ElasticsearchConfigurationSupport {
+    
+    @Value("${elasticsearch.host:localhost}")
+    private String esHost;
+
+    @Value("${elasticsearch.port:9200}")
+    private int esPort;
+
+    
+    @Bean
+    public ElasticsearchTransport elasticsearchTransport() {
+        RestClient restClient = RestClient.builder(
+                new HttpHost(esHost, esPort))
+            .build();
+        return new RestClientTransport(restClient, new JacksonJsonpMapper());
+    }
+     
+    @Bean
+    public ElasticsearchClient elasticsearchClient(ElasticsearchTransport transport) {
+        return new ElasticsearchClient(transport);
+    }
+
+    @Bean
+    public ElasticsearchTemplate elasticsearchTemplate(
+            ElasticsearchClient client, 
+            ElasticsearchConverter converter
+    ) {
+        return new ElasticsearchTemplate(client, converter);
+    }
+
+}
