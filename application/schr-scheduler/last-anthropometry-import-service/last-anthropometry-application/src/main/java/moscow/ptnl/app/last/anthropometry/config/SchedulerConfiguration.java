7f3f3103e7388d7f599465e48cd223a16bf33fbc
Данил Дрючин <danil.dryuchin@ptnl.moscow>
2023-03-01 13:47:04 +0300

    SCHREGISTER-20 Разработка планировщика erpChangePatientPersonalDataImportService

--- application/schr-scheduler/last-anthropometry-import-service/last-anthropometry-application/src/main/java/moscow/ptnl/app/last/anthropometry/config/SchedulerConfiguration.java
+++ application/schr-scheduler/last-anthropometry-import-service/last-anthropometry-application/src/main/java/moscow/ptnl/app/last/anthropometry/config/SchedulerConfiguration.java
@@ -20,7 +20,7 @@ import java.time.Duration;
  */
 @Configuration
 @EnableScheduling
-public class SchedulerConfiguration implements SchedulingConfigurer  {
+public class SchedulerConfiguration implements SchedulingConfigurer {
 
     protected static final Logger LOG = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass().getName());
 
