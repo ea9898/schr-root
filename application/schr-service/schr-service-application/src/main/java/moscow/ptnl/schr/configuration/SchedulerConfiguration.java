e5fb883eb4cda1e3f29984e509ce5f9b782e5e95
m.kachalov <mike74@mail.ru>
2023-03-29 13:23:54 +0300

    SCHREGISTER-83 Метод searchByDsl. Нет проверки данных из системного параметра services.security.settings

---
+++ application/schr-service/schr-service-application/src/main/java/moscow/ptnl/schr/configuration/SchedulerConfiguration.java
@@ -0,0 +1,14 @@
+package moscow.ptnl.schr.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+/**
+ *
+ * @author m.kachalov
+ */
+@Configuration
+@EnableScheduling
+public class SchedulerConfiguration {
+    
+}
